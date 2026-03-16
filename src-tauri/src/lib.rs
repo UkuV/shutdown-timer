412ecf7f17e59a2347003fe09e466b389f56c75e
Uku Vesper <uku.vesper@pipedrive.com>
2026-03-16 11:34:06 +0200

    feat: add tauri-plugin-tray dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,6 +17,7 @@ fn cancel_shutdown() {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_tray::init())
         .invoke_handler(tauri::generate_handler![start_shutdown, cancel_shutdown])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
